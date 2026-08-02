08921ae24b5a2c354d81862bf71838ef8fcb3c86
jainyindhu <jainyindhu1617@gmail.com>
2026-08-02 20:49:12 +0530

    Create half_adder_tb.v

---
+++ half_adder_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns/1ps
+
+module half_adder_tb;
+
+reg A;
+reg B;
+wire Sum;
+wire Carry;
+
+half_adder uut (
+    .A(A),
+    .B(B),
+    .Sum(Sum),
+    .Carry(Carry)
+);
+
+initial begin
+
+    $monitor("Time=%0t | A=%b B=%b | Sum=%b Carry=%b",
+             $time, A, B, Sum, Carry);
+
+    A = 0; B = 0;
+    #10;
+
+    A = 0; B = 1;
+    #10;
+
+    A = 1; B = 0;
+    #10;
+
+    A = 1; B = 1;
+    #10;
+
+    $finish;
+end
+
+endmodule
